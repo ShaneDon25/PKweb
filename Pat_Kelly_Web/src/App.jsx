9e5331d66393ac6b5d59a92c2228972deac667cd
ShaneDon25 <shanedonwork2017@gmail.com>
2025-06-28 21:25:29 +0100

    Just some testing with the java script for design and preparing nav bar for later use

--- Pat_Kelly_Web/src/App.jsx
+++ Pat_Kelly_Web/src/App.jsx
@@ -8,12 +8,20 @@ function App() {
 
   return (
     <>
+      <nav className="navbar">
+
+      </nav>
+      
       <div>
+        
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src="/vite.svg" className="logo" alt="Vite logo" />
         </a>
       </div>
       <h1>Vite + React</h1>
